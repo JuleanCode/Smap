afee729a07c3c437e8dc175178d0eb914e5291ae
Julean Hommel <juleanhommel@gmail.com>
2021-12-09 12:32:26 +0100

    Add project files.

---
+++ Smap/Smap/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Smap.Services;
+using Smap.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smap
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
