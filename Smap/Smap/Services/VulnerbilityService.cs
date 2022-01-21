f53cda8abc3623a03cd444fc3148a84045d59c3e
JuleanCode <juleanhommel@gmail.com>
2022-01-21 19:24:23 +0100

    added function to get the vulnerbility (hard coded)

---
+++ Smap/Smap/Services/VulnerbilityService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using SQLite;
+using Smap.Models;
+
+namespace Smap.Services
+{
+    public class VulnerbilityService
+    {
+        static SQLiteConnection db;
+        static void Init()
+        {
+            if (db != null)
+                return;
+
+            var dataBasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "SmapDB.db");
+
+            db = new SQLiteConnection(dataBasePath);
+
+            db.CreateTable<Ip>();
+            db.CreateTable<OpenPort>();
+            db.CreateTable<Report>();
+            db.CreateTable<Network>();
+            db.CreateTable<Vulnerbility>();
+            db.CreateTable<User>();
+        }
+
+        public static Vulnerbility GetVulnerbility()
+        {
+            Vulnerbility vulnerbility = new Vulnerbility
+            {
+                Id = 1,
+                Date = DateTime.Now,
+                Cve = "CVE-2010-1219"
+            };
+
+            return vulnerbility;
+        }
+    }
+}
